0f1f2d1c94075e91ecf75b1774e507c4da0c758e
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2016-03-08 15:05:07 +0000

    Rendering of template and move to output

--- performancepage.py
+++ performancepage.py
@@ -1,10 +1,48 @@
-import yaml
+import os
 import json
+import shutil
+
+import yaml
+from jinja2 import Environment, FileSystemLoader
+
+
+PATH = os.getcwd()
 
 with open('conf.yml', 'r') as f:
     string = f.read()
     conf = yaml.load(string)
 
+def output_html(text):
+    if not os.path.isdir(os.path.join(PATH, 'output')):
+        os.makedirs(os.path.join(PATH, 'output'))
+
+    with open('output/index.html', 'w') as f:
+        f.write(text)
+
+def copy_assets(files):
+    """takes a list of files or directories and copies to output
+    """
+    for f in files:
+        if os.path.isdir(os.path.join(PATH, f)):
+            try:
+                shutil.copytree(os.path.join(PATH, f), os.path.join(PATH, 'output', f))
+            except OSError:
+                shutil.rmtree(os.path.join(PATH, 'output', f))
+                shutil.copytree(os.path.join(PATH, f), os.path.join(PATH, 'output', f))
+        else:
+            shutil.copy(os.path.join(PATH, f), os.path.join(PATH, 'output', f))
+
+
 
 # Metrics grouped together.
 # First in group get special treatment.
+
+if __name__ == '__main__':
+    env = Environment(loader=FileSystemLoader('templates'))
+    template = env.get_template('index.html')
+    data = {'test': 'jinja works'}
+    html = template.render(templates_folder='templates', **data)
+    output_html(html)
+
+    assets = ['css', 'js', 'LICENSE']
+    copy_assets(assets)
