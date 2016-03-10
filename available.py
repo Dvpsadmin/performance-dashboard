4984d4183ea8582b645ebf89c7b5353792378a60
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2016-03-10 18:49:04 +0000

    Generate a markdown to see all available metrics

--- available.py
+++ available.py
@@ -1,1 +1,20 @@
 # Read the conf.yaml and ask what metrics you want to output in common.
+import codecs
+import os
+
+import yaml
+from datawrapper import DataWrapper
+
+with codecs.open('conf.yml', 'r') as f:
+    conf = yaml.load(f.read())
+
+
+if __name__ == '__main__':
+    try:
+        token = os.env['SD_TOKEN']
+    except KeyError:
+        token = raw_input('What is your token for Server Density: ').strip()
+        os.env['SD_TOKEN'] = token
+
+    data = DataWrapper(token, conf)
+    data.available() # generates a file called available.md
